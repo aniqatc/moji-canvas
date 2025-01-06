da2c4bc9515b9070d8b0127bcda102cedba5f685
aniqatc <aniqatc97@gmail.com>
2025-01-05 19:34:16 -0500

    add fonts, update configs, remove placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Alien } from 'phosphor-react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(10);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="mx-auto flex min-h-screen w-full flex-col items-center justify-center gap-3 bg-green-50">
+      <h1 className="font-display text-4xl">moji canvas</h1>
+      <button
+        className="flex items-center gap-1 rounded-lg bg-gradient-to-br from-green-400 to-blue-600 px-5 py-2.5 text-center font-handwritten text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-green-200"
+        onClick={() => setCount((count) => count + 10)}
+      >
+        <Alien size={24} weight="light" /> count is {count}
+      </button>
+      <p className="w-[32ch] text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+        dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.{' '}
       </p>
-    </>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          'system-ui',
+          'Avenir',
+          'Helvetica',
+          'Arial',
+          'sans-serif',
+        ],
+        mono: [
+          'DM Mono',
+          'ui-monospace',
+          'SFMono-Regular',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          'Liberation Mono',
+          'Courier New',
+          'monospace',
+        ],
+        display: ['Agbalumo', 'sans-serif'],
+        handwritten: ['Shantell Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
