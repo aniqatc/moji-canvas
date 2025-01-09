4db54596134a64c0d2133d68b53d1be8998f36f3
aniqatc <aniqatc97@gmail.com>
2025-01-09 17:02:08 -0500

    add metadata in json format + upload svg stickers

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,27 @@ import { useState } from 'react';
 import { Alien } from 'phosphor-react';
 
 function App() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="mx-auto flex min-h-screen w-full flex-col items-center justify-center gap-3 bg-green-50">
-      <h1 className="font-display text-4xl">moji canvas</h1>
+    <div className="mx-auto flex min-h-screen w-full flex-col items-center justify-center gap-5"
+    style={{
+        backgroundColor: "#ffefef",
+        backgroundImage:  "radial-gradient(#ec1111 2px, transparent 2px), " +
+            "radial-gradient(#ec1111 2px, #ffefef 2px)",
+        backgroundSize: "100px 100px",
+        backgroundPosition: "0 0, 50px 50px"
+    }}>
+      <h1 className="font-display text-6xl text-[#540F0F]">moji canvas</h1>
       <button
-        className="flex items-center gap-1 rounded-lg bg-gradient-to-br from-green-400 to-blue-600 px-5 py-2.5 text-center font-handwritten text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-green-200"
-        onClick={() => setCount((count) => count + 10)}
+        className="text-white bg-gradient-to-br from-pink-500 to-orange-400
+        hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200
+        font-medium rounded-lg text-sm px-5 py-2.5 flex items-center gap-1"
+        onClick={() => setCount((count) => count + 5)}
       >
         <Alien size={24} weight="light" /> count is {count}
       </button>
-      <p className="w-[32ch] text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
-        dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit.{' '}
-      </p>
     </div>
   );
 }
-
 export default App;
