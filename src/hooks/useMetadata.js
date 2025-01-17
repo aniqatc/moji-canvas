f866382c36f9b7de4e3f743c2b2efecde0dda6d4
aniqatc <aniqatc97@gmail.com>
2025-01-16 21:02:30 -0500

    first custom hook 🪝

---
+++ src/hooks/useMetadata.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function useMetadata() {
+    const [metadata, setMetadata] = useState(null);
+
+    useEffect(() => {
+        async function getMetadata() {
+            const response = await fetch(`/data/metadata.json`);
+            const data = await response.json();
+            setMetadata(data);
+        }
+        getMetadata();
+    }, []);
+
+    return metadata;
+}
