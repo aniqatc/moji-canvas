d21ab3083fa7d409cea77d9f13abea5e78ba36a6
aniqatc <aniqatc97@gmail.com>
2025-01-22 16:45:06 -0500

    format

--- src/components/reusable/Modal.jsx
+++ src/components/reusable/Modal.jsx
@@ -23,28 +23,28 @@ export default function Modal({ heading, children, isOpen, onClose }) {
           onClick={handleModalClick}
         >
           <FocusLock returnFocus={true}>
-          <motion.div
-            aria-modal="true"
-            initial={{ scale: 0.9, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.9, opacity: 0 }}
-            transition={{ duration: 0.3 }}
-            className="w-[500px] cursor-default rounded bg-white px-5 py-4 shadow-xl h-sm:max-w-[90%]"
-          >
-            <div className="mb-2 flex items-center justify-between border-b pb-2">
-              <p className="flex items-center text-lg font-semibold">
-                <img src={all} className="size-8" />
-                {heading}
-              </p>
-              <button
-                onClick={() => onClose()}
-                className="origin-center cursor-pointer text-gray-500 transition-all duration-500 hover:rotate-[180deg] hover:text-gray-900"
-              >
-                <X weight="bold" size="24" />
-              </button>
-            </div>
-            <div className="text-[13px] text-gray-800">{children}</div>
-          </motion.div>
+            <motion.div
+              aria-modal="true"
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.9, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className="w-[500px] cursor-default rounded bg-white px-5 py-4 shadow-xl h-sm:max-w-[90%]"
+            >
+              <div className="mb-2 flex items-center justify-between border-b pb-2">
+                <p className="flex items-center text-lg font-semibold">
+                  <img src={all} className="size-8" />
+                  {heading}
+                </p>
+                <button
+                  onClick={() => onClose()}
+                  className="origin-center cursor-pointer text-gray-500 transition-all duration-500 hover:rotate-[180deg] hover:text-gray-900"
+                >
+                  <X weight="bold" size="24" />
+                </button>
+              </div>
+              <div className="text-[13px] text-gray-800">{children}</div>
+            </motion.div>
           </FocusLock>
         </motion.div>
       )}
